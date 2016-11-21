ffe3a6bae87231f0cf556506ac3ce0eee9e92f26
sourcecode121 <sourcecode121@users.noreply.github.com>
2016-11-21 17:59:03 +0530

    Change map types

--- app/src/main/java/com/example/maps/MainActivity.java
+++ app/src/main/java/com/example/maps/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.maps;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+    private boolean mapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.map_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mapReady) googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        });
+
+        findViewById(R.id.satellite_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mapReady) googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            }
+        });
+
+        findViewById(R.id.hybrid_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mapReady) googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        mapReady = true;
+        LatLng landmark = new LatLng(37.4220041, -122.0862462);
+        CameraPosition cameraPosition = CameraPosition.builder()
+                                                    .target(landmark)
+                                                    .zoom(17)
+                                                    .build();
+        this.googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
