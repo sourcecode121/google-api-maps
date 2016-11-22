74b7a2cb9fc71d7d57e0b1e3dc0c248bf0de5dd3
sourcecode121 <sourcecode121@users.noreply.github.com>
2016-11-22 14:03:35 +0530

    Adding circles and polylines

--- app/src/main/java/com/example/maps/AnimateMapsActivity.java
+++ app/src/main/java/com/example/maps/AnimateMapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.maps;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,9 +10,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class AnimateMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -21,15 +25,27 @@ public class AnimateMapsActivity extends AppCompatActivity implements OnMapReady
     private MarkerOptions london;
     private MarkerOptions losAngeles;
     private MarkerOptions newYork;
+    private CircleOptions londonCircle;
+    private CircleOptions losAngelesCircle;
+    private CircleOptions newYorkCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template);
 
-        london = new MarkerOptions().position(Constants.LONDON).title(getString(R.string.london));
-        losAngeles = new MarkerOptions().position(Constants.LA).title(getString(R.string.los_angeles));
-        newYork = new MarkerOptions().position(Constants.NEW_YORK).title(getString(R.string.new_york));
+        // Create markers
+        london = new MarkerOptions().position(Constants.LONDON).title(getString(R.string.london))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
+        losAngeles = new MarkerOptions().position(Constants.LA).title(getString(R.string.los_angeles))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
+        newYork = new MarkerOptions().position(Constants.NEW_YORK).title(getString(R.string.new_york))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
+
+        // Create circles
+        londonCircle = new CircleOptions().center(Constants.LONDON).radius(50000).strokeColor(Color.GREEN);
+        losAngelesCircle = new CircleOptions().center(Constants.LA).radius(50000).strokeColor(Color.GREEN);
+        newYorkCircle = new CircleOptions().center(Constants.NEW_YORK).radius(50000).strokeColor(Color.GREEN);
 
         Button button1 = (Button) findViewById(R.id.button1);
         Button button2 = (Button) findViewById(R.id.button2);
@@ -71,9 +87,21 @@ public class AnimateMapsActivity extends AppCompatActivity implements OnMapReady
     @Override
     public void onMapReady(GoogleMap map) {
         googleMap = map;
+
+        // Add markers
         googleMap.addMarker(london);
         googleMap.addMarker(losAngeles);
         googleMap.addMarker(newYork);
+
+        // Add polylines
+        googleMap.addPolyline(new PolylineOptions().geodesic(true)
+                .add(Constants.LONDON).add(Constants.NEW_YORK).add(Constants.LA));
+
+        // Add circles
+        googleMap.addCircle(londonCircle);
+        googleMap.addCircle(losAngelesCircle);
+        googleMap.addCircle(newYorkCircle);
+
         mapReady = true;
         cameraPosition = CameraPosition.builder().target(Constants.LONDON).zoom(7).build();
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
